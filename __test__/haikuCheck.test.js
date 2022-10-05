d8cd5b4942ddc9f82b388f99dc96b338c9cd892c
Alexander <knibz@protonmail.com>
2022-10-04 23:34:09 -0400

    add passing vowel and diphthong counter tests

---
+++ __test__/haikuCheck.test.js
@@ -0,0 +1,14 @@
+import { TestScheduler } from 'jest'
+import { vowelCounter, diphthongCounter } from '../src/haikuCheck.js'
+
+describe ('vowelCounter', () => {
+  test('should return the number of vowels in a string', () => {
+    expect(vowelCounter("I want all the vowels")).toEqual(6);
+  });
+});
+
+describe ('diphthongCounter', () => {
+  test('should return the number of diphthongs in a string', () => {
+    expect(diphthongCounter("Coy boy toy")).toEqual(3);
+  });
+});
