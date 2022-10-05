dcf5f45b40f23422f677785bf880a594ef910dc3
Alexander <knibz@protonmail.com>
2022-10-04 23:17:21 -0400

    add diphthongCounter function that uses regex

--- src/haikuCheck.js
+++ src/haikuCheck.js
@@ -1,4 +1,8 @@
 export const vowelCounter = (string) => {
   return (/[aeiouy]/gi).test(string) ? string.match(/[aeiouy]/gi).length : 0;
-}
+};
+
+export const diphthongCounter = (string) => {
+  return (/[aeiouy][aeiouy]/gi).test(string) ? string.match(/[aeiouy][aeiouy]/gi).length : 0;
+};
 
