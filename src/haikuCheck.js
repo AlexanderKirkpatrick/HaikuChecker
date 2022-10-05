a14bff972b43fb0f675d41a3331f0766c5d3ef51
Alexander <knibz@protonmail.com>
2022-10-05 00:05:50 -0400

    add silentVowelCounter function that uses regex

--- src/haikuCheck.js
+++ src/haikuCheck.js
@@ -6,3 +6,11 @@ export const diphthongCounter = (string) => {
   return (/[aeiouy][aeiouy]/gi).test(string) ? string.match(/[aeiouy][aeiouy]/gi).length : 0;
 };
 
+export const silentVowelCounter = (string) => {
+  const silentA = (/ally\b/gi).test(string) ? string.match(/ally\b/gi).length : 0;
+  const silentE = (/e\b/gi).test(string) ? string.match(/e\b/gi).length : 0;
+  const silentED = (/ed\b/gi).test(string) ? string.match(/ed\b/gi).length : 0;
+  const silentU = (/gu[aeiouy]/gi).test(string) ? string.match(/gu[aeiouy]/gi).length : 0;
+  const silentI = (/business/gi).test(string) ? string.match(/business/gi).length : 0;
+  return silentA + silentE + silentED + silentU + silentI;
+};
