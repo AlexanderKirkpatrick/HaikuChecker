913812b0a25f0c4858ff41fe05ea89fb7cf78aaf
Alexander <knibz@protonmail.com>
2022-10-05 22:34:36 -0400

    add passing silentVowel test

--- __test__/haikuCheck.test.js
+++ __test__/haikuCheck.test.js
@@ -1,5 +1,5 @@
 import { TestScheduler } from 'jest'
-import { vowelCounter, diphthongCounter } from '../src/haikuCheck.js'
+import { vowelCounter, diphthongCounter, silentVowelCounter } from '../src/haikuCheck.js'
 
 describe ('vowelCounter', () => {
   test('should return the number of vowels in a string', () => {
@@ -10,5 +10,11 @@ describe ('vowelCounter', () => {
 describe ('diphthongCounter', () => {
   test('should return the number of diphthongs in a string', () => {
     expect(diphthongCounter("Coy boy toy")).toEqual(3);
+  });
+});
+
+describe ('silentVowelCounter', () => {
+  test('should return the number of silent vowels in a string', () => {
+    expect(silentVowelCounter("bread taste guitar business artistically age")).toEqual(5);
   });
 });
